85d98e691b6537fe66ad1aeb1429443e7c20fa8b
Apricityx <78242039+Apricityx@users.noreply.github.com>
2022-05-02 20:28:11 +0800

    Update CMASettings.java

--- src/main/java/tech/mctown/cma/CMASettings.java
+++ src/main/java/tech/mctown/cma/CMASettings.java
@@ -8,14 +8,14 @@ public class CMASettings {
     static final String CMA = CMAExtension.MODID;
 
     @Rule(
-            desc = "Execute the command on the sign if it's right-clicked, the player is not sneaking nor holding " +
-                    "anything in the main hand and the text starts with '/'.",
+            desc = "Execute the command on the sign if it's right-clicked by player who is not sneaking nor holding " +
+                    "anything in the main hand,and texts must start with '/'.",
             category = {CMA, SURVIVAL}
     )
     public static boolean runCommandOnSign = false;
 
     @Rule(
-            desc = "Player can edit the sign when it's right-clicked, the player is sneaking and not holding anything " +
+            desc = "Player can edit the sign when it's right-clicked if the player is sneaking and not holding anything " +
                     "in the main hand.",
             category = {CMA, SURVIVAL}
     )
