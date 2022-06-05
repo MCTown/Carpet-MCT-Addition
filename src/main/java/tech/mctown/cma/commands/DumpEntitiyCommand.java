23b3855da63241b08b72b47545f2e43d2c4cd2f9
xiaoyu2006 <caoyi06@qq.com>
2022-06-05 01:03:01 +0000

    Remove copied canUseCommand

--- src/main/java/tech/mctown/cma/commands/DumpEntitiyCommand.java
+++ src/main/java/tech/mctown/cma/commands/DumpEntitiyCommand.java
@@ -1,5 +1,6 @@
 package tech.mctown.cma.commands;
 
+import carpet.settings.SettingsManager;
 import carpet.utils.Messenger;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
@@ -12,20 +13,13 @@ import net.minecraft.text.Text;
 import net.minecraft.text.Texts;
 import org.lwjgl.system.CallbackI;
 import tech.mctown.cma.CMASettings;
-import tech.mctown.cma.utils.CarpetModUtil;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import static net.minecraft.server.command.CommandManager.literal;
 
 public class DumpEntitiyCommand {
     public static void registerCommand(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(
-                literal("dumpentity").requires(src -> CarpetModUtil.canUseCommand(src, CMASettings.commandDumpEntity))
+                literal("dumpentity").requires(src -> SettingsManager.canUseCommand(src, CMASettings.commandDumpEntity))
                         .executes(DumpEntitiyCommand::dumpEntity)
         );
     }
@@ -33,7 +27,6 @@ public class DumpEntitiyCommand {
     public static int dumpEntity(CommandContext<ServerCommandSource> ctx) throws CommandSyntaxException {
         ServerCommandSource src = ctx.getSource();
         ServerWorld world = src.getWorld();
-        ArrayList<BaseText> l = new ArrayList<>();
         for (Entity e : world.iterateEntities()) {
             Messenger.m(src, Text.of(e.toString()));
         }
