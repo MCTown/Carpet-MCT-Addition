f782e6bf5f333d499ecbd90d7217418d988eebec
xiaoyu2006 <caoyi06@qq.com>
2022-07-11 05:00:10 +0000

    Checkpoint

--- src/main/java/tech/mctown/cma/mixins/fakePlayerGamemode/PlayerCommandMixin.java
+++ src/main/java/tech/mctown/cma/mixins/fakePlayerGamemode/PlayerCommandMixin.java
@@ -1,11 +1,12 @@
-package tech.mctown.cma.mixins.noCreativeFakePlayers;
+package tech.mctown.cma.mixins.fakePlayerGamemode;
 
 import carpet.commands.PlayerCommand;
 import carpet.utils.Messenger;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.world.GameMode;
+import tech.mctown.cma.CMASettings;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,13 +15,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(PlayerCommand.class)
 public abstract class PlayerCommandMixin {
     @Inject(method = "cantSpawn", at = @At("HEAD"), remap = false)
-    void checkGamemode(CommandContext<ServerCommandSource> context, CallbackInfoReturnable<Boolean> ci) {
+    static private void checkGamemode(CommandContext<ServerCommandSource> context, CallbackInfoReturnable<Boolean> ci) {
         try {
             StringArgumentType.getString(context, "gamemode");
-        } catch (IllegalArgumentException var10) {
+        } catch (IllegalArgumentException e) {
             return;
         }
-        ci.setReturnValue(false);
-
+        if (!CMASettings.fakePlayerGamemode) {
+            ci.setReturnValue(true);
+        }
     }
 }
